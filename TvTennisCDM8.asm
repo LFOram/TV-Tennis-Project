c6cf1c7e2eaa722aa7596f02c1803a7326ad3e97
3eak <stefan.cooper27@gmail.com>
2017-03-23 21:55:45 +0000

    TvTennisCDM8 0.01
    - Representing Ball Velocity (page 6)
    
    not sure the reasoning for this but i have followed the instructions
    from page 6 on the project proposal
    
    Representing ball velocity:
    The velocity, vxy, of the ball has a horizontal (x) component, vx, and
    a vertical (y)
    component, vy. Each of these is represented by a 3-bit 2’s complement
    (signed) number,
    meaning that vx and vy can each take on values in the range -4..+3.
    This is a very coarse
    scale, but it is good enough for our purposes.
    A positive x velocity means that the ball is travelling from left to
    right across the space, and
    a positive y velocity means that the ball is travelling from bottom to
    top. Negative velocities
    of course mean that the ball is travelling in the opposite direction.
    Any combination of vx
    and vy is possible.
    Every data item read by the CdM-8 core must be represented as an 8-bit
    string, because
    CdM-8 is an 8-bit platform. Rather than pad each of the two velocity
    components to 8 bits
    we combine them into one 8-bit string as follows: bits 0..2 carry vx,
    bits 3..5 carry vy, and
    bits 6..7 are always set to 0.

---
+++ TvTennisCDM8.asm
@@ -0,0 +1,40 @@
+asect 0x00
+
+
+# REPRESENTING BALL VELOCITY. PAGE 6.
+
+ldi r0,vy
+ld r0,r0
+
+ldi r1,vx
+ld r1,r1
+
+move r0,r2
+
+shl r1
+shl r1   # Rather than pad each of the two velocity components 
+shl r1   # to 8 bits we combine them into one 8-bit string 
+shl r1   # as follows: bits 0..2 carry vx, bits 3..5 carry vy, and
+shl r1   # bits 6..7 are always set to 0.
+shl r1
+
+
+shl r2
+shl r1
+shl r2
+shl r1
+shl r2
+
+
+
+
+
+halt
+
+inputs>
+vx: dc 0b00000101 # THIS INPUT WILL BE TAKEN FROM LOGISIM
+vy: dc 0b00000111 # THIS INPUT WILL BE TAKEN FROM LOGISIM
+endinputs>
+
+
+end
