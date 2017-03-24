3ea5571546403e2f1067e7015ec9985e746755f3
3eak <stefan.cooper27@gmail.com>
2017-03-24 16:51:20 +0000

    -More Efficient, switched sides

--- TvTennisCDM8.asm
+++ TvTennisCDM8.asm
@@ -13,24 +13,14 @@ ld r0,r0
 ldi r1,vx
 ld r1,r1
 
-move r0,r2
-
-shl r1
-shl r1   # Rather than pad each of the two velocity components 
-shl r1   # to 8 bits we combine them into one 8-bit string 
-shl r1   # as follows: bits 0..2 carry vx, bits 3..5 carry vy, and
-shl r1   # bits 6..7 are always set to 0.
-shl r1
-
-
-shl r2
-shl r1
-shl r2
-shl r1
-shl r2
+shr r0
+shr r0   # Rather than pad each of the two velocity components 
+shr r0   # to 8 bits we combine them into one 8-bit string    
+or r0,r1 # as follows: bits 0..2 carry vx, bits 3..5 carry vy, and 
+         # bits 6..7 are always set to 0.
 
 ldi r3,vxy
-st r3,r2
+st r3,r1
 
 #----------------------------------------------------------------
 
@@ -44,8 +34,8 @@ st r3,r2
 halt
 
 inputs>
-vx: dc 0b00000101 # THIS INPUT WILL BE TAKEN FROM LOGISIM
-vy: dc 0b00000111 # THIS INPUT WILL BE TAKEN FROM LOGISIM
+vx: dc 0b10100000 # THIS INPUT WILL BE TAKEN FROM LOGISIM
+vy: dc 0b11100000 # THIS INPUT WILL BE TAKEN FROM LOGISIM
 endinputs>
 
 vxy: ds 1
