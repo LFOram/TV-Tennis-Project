efe2cfad80bbbe3c59a468a25c61537ced2aece6
3eak <stefan.cooper27@gmail.com>
2017-05-12 11:20:12 +0100

    Rework started with new variables. Hype
    -Replaced all hard coded variables with appropriate variables.
    -re worked some functions of optimizations

--- Pong Source 2 Software.asm
+++ Pong Source 2 Software.asm
@@ -7,13 +7,33 @@
 # FD rscore
 # FE ballx
 # FF bally
-
-
+#
+#
+#
+# D8 keyboard-codev
+# D9 
+# DA lpad-codev
+# DB rpad-codev
+# DC lscore-codev
+# DD rscore-codev
+# DE ballx-codev
+# DF bally-codev
+#
+#
+# E0 Direction
+# E1 XSpeed: dc 4
+# E2 YAngle: dc 0
+#
+#
 # 
 
 
 asect 0x00
 
+
+ldi r0,4
+ldi r1,0xE1
+st r1,r0
 #-----------------------------MAIN-----------------------------------
 
 reset:
@@ -27,7 +47,7 @@ jsr scoreboardReset
 main0:
 
 
-ldi r0,direction # load current direction of b
+ldi r0,0xE0 # load current direction of b
 ld r0,r0
 ldi r1,0
 cmp r1,r0
@@ -52,7 +72,7 @@ br main0
 
 checkIfScore:
 
-ldi r0,0xFE
+ldi r0,0xDE
 ld r0,r0
 ldi r1,0
 cmp r0,r1
@@ -64,31 +84,40 @@ rts
 
 scoreLeft:
 
-ldi r0,0xFD
+ldi r0,0xDD
 ld r0,r1
 inc r1
+st r0,r1
+ldi r0,0xFD
 st r0,r1
 rts
 
 
 scoreRight:
 
-ldi r0,0xFC
+ldi r0,0xDC
 ld r0,r1
 inc r1
+st r0,r1
+ldi r0,0xFC
 st r0,r1
 rts
 
 
 scoreboardReset:
 
-ldi r0,0xFC
+ldi r0,0xDC
 ld r0,r0
 ldi r1,0
 st r0,r1
-ldi r0,0xFD
+ldi r0,0xFC
+st r0,r1
+ldi r0,0xDD
 ld r0,r0
 st r0,r1
+ldi r0,0xFD
+st r0,r1
+
 rts
 
 
@@ -99,23 +128,27 @@ rts
 
 ballReset:
 
-ldi r0,0xFE
+ldi r0,0xDE
 ldi r1,initialBallPositionX
-ld r1,r1
+ldc r1,r1
+st r0,r1
+ldi r0,0xFE
+st r0,r1
+ldi r0,0xDF
+ldi r1,initialBallPositionY
+ldc r1,r1
 st r0,r1
 ldi r0,0xFF
-ldi r1,initialBallPositionY
-ld r1,r1
 st r0,r1
 rts
 
 ballMove:
 
-ldi r0,0xFE
+ldi r0,0xDE
 ld r0,r0
-ldi r1,0xFF
+ldi r1,0xDF
 ld r1,r1
-ldi r2,direction
+ldi r2,0xE0
 ld r2,r2
 clr r3
 cmp r2,r3
@@ -125,44 +158,50 @@ br ballLeft
 
 ballRight:
 
-ldi r0,XSpeed
+ldi r0,0xE1
 ld r0,r0
-ldi r1,YAngle
+ldi r1,0xE2
 ld r1,r1
 
-ldi r2,0xFE # load x coord ball
+ldi r2,0xDE # load x coord ball
 ld r2,r2
 add r2,r0
 st r2,r0  # store new x coord
+ldi r2,0xFE
+st r2,r0
 
-ldi r2,0xFF # load y coord
+ldi r2,0xDF # load y coord
 ld r2,r2
 add r2,r1
 st r2,r1 # st new coord
+ldi r2,0xFF
+st r2,r1
 br main1
 
 
 
 ballLeft:
 
-ldi r0,XSpeed 
+ldi r0,0xE1
 ld r0,r0
 neg r0
-ldi r1,YAngle
+ldi r1,0xE2
 ld r1,r1
 
 
-ldi r2,0xFE # load x coord ball
+ldi r2,0xDE # load x coord ball
 ld r2,r3
 add r3,r0
-ldi r2,0xFE
 st r2,r0 # st new x coord
+ldi r2,0xFE
+st r2,r0
 
-ldi r2,0xFF # load y coord ball
+ldi r2,0xDF # load y coord ball
 ld r2,r3
 add r3,r1
-ldi r2,0xFF
 st r2,r1 # st new y coord
+ldi r2,0xFF
+st r2,r1
 br main1
 
 
@@ -170,76 +209,56 @@ br main1
 
 initialHitDetect:
 
-ldi r0,0xFE
+ldi r0,0xDE
 ld r0,r0
 
 ldi r1,1         #*********** check 
 cmp r0,r1
-beq hitDetectL
+beq hitDetectLoadL
 ldi r1,254    # check if ball is one away from board edge
 cmp r0,r1
-beq hitDetectR
+beq hitDetectLoadR
 rts
 
 
 
 
+hitDetectLoadL:
 
-hitDetectL:
-
-ldi r0,0xFA
+ldi r0,0xDA
 ld r0,r0
+br hitDetect
+rts
 
-ldi r2,0xFF
+hitDetectLoadR:
+
+ldi r0,0xDB
+ld r0,r0
+br hitDetect
+rts
+
+
+hitDetect:
+
+ldi r2,0xDF
 ld r2,r2
 ldi r1,8
 sub r2,r1
 add r1,r2 # get both sides of paddle
 
 cmp r0,r1 # if ball is higher than lowest part of paddle, continue
-bhi secondCheckL
+bhi secondCheck
 rts
-secondCheckL:
+secondCheck:
 cmp r0,r2 # if ball is lower than top of paddle, continue
-blo hitDetectedL
+blo hitDetected
 rts
-hitDetectedL:
-ldi r0,direction
-ldi r1,1
+hitDetected:
+ldi r0,0xE0
+ld r0,r1
+not r1
 st r0,r1
 rts
-
-
-
-hitDetectR:
-
-ldi r0,0xFB
-ld r0,r0
-
-ldi r2,0xFF
-ld r2,r2
-ldi r1,8
-sub r2,r1
-add r1,r2 # get both sides of paddle
-cmp r0,r1 # if ball is higher than lowest part of paddle, continue
-bhi secondCheckR
-rts
-secondCheckR:
-cmp r0,r2 # if ball is lower than top of paddle, continue
-blo hitDetectedR
-rts
-hitDetectedR:
-ldi r0,direction
-ldi r1,0
-st r0,r1
-rts
-
-
-
-
-
-
-
 
 
 
@@ -248,9 +267,14 @@ rts
 
 paddleReset:
 
-ldi r0,0xFA 
-ldi r2,0xFB
+ldi r0,0xDA 
+ldi r2,0xDB
 ldi r1,initialPaddlePosition
+ldc r1,r1
+st r0,r1
+st r2,r1
+ldi r0,0xFA
+ldi r2,0xFB
 st r0,r1
 st r2,r1
 rts 
@@ -275,14 +299,16 @@ beq upInput
 cmp r0,r3
 beq downInput
 
-	noMove:
+noMove:
 
-	ldi r0,0xFA # load paddle position
+	ldi r0,0xDA # load paddle position
 	ld r0,r1
+	st r0,r1
+	ldi r0,0xFA
 	st r0,r1
 	rts
 	
-	upInput:
+upInput:
 	
 	ldi r0,0xFA # load paddle position
 	ld r0,r0 #  load paddle
@@ -293,11 +319,13 @@ beq downInput
 	ldi r1,255 # load max board 0,255
 	cmp r0,r1 # compare top paddle> max board
 	bhi noMove # if it is bigger, then dont move the board
-	ldi r0,0xFA
+	ldi r0,0xDA
 	st r0,r3 # store new paddle middle position in 0xFA
+	ldi r0,0xFA
+	st r0,r3
 	rts
 
-	downInput:
+downInput:
 
 	ldi r0,0xFA # load paddle position
 	ld r0,r0 #  load paddle
@@ -308,8 +336,10 @@ beq downInput
 	ldi r1,0 # load lowest board 0,0
 	cmp r0,r1 # compare top paddle> max board
 	blo noMove # if it is bigger, then dont move the board
-	ldi r0,0xFA
+	ldi r0,0xDA
 	st r0,r3 # store new paddle middle position in 0xFA
+	ldi r0,0xFA
+	st r0,r3
 	rts
 
 
@@ -327,9 +357,6 @@ halt
 initialBallPositionX: dc 128
 initialBallPositionY: dc 128
 initialPaddlePosition: dc 128
-XSpeed: dc 4
-YAngle: dc 0
-direction: dc 0
 
 end
 
